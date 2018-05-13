657241c23c6d8c2b06535cb15a3e13d2de44bdbe
cvmartins1996 <cvmartins1996@gmail.com>
2018-05-13 00:05:03 -0300

    update realizado

--- src/main/java/io/spring/crud/cartorio/controller/CartorioController.java
+++ src/main/java/io/spring/crud/cartorio/controller/CartorioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -49,21 +50,27 @@ public class CartorioController {
 		return "browser";
 	}
 	
-	@RequestMapping(value = "/cartorio/editar", method = RequestMethod.GET)
-	public String update(@ModelAttribute Long id, Model model) {
+	@RequestMapping(value = "/cartorio-editar{id}", method = RequestMethod.GET)
+	public String update(@PathVariable Long id, Model model) {
 		model.addAttribute("cartorio", cartorioService.getCartorio(id));
 		return "update";
 	}
 	
-	@RequestMapping(value = "/cartorio/salvar", method = RequestMethod.POST)
-	public String salvarUpdate() {
-		System.out.println("stop");
+	@RequestMapping(value = "/cartorio-salvar", method = RequestMethod.POST)
+	public String salvarUpdate(@ModelAttribute Cartorio cartorio, Model model) {
+		try {
+			cartorioService.updateCartorio(cartorio);
+			model.addAttribute("cartorios", cartorioService.getAllCartorios());
+		} catch (Exception e) {
+			return "update";
+		}
 		return "browser";
 	}
 	
-	@RequestMapping(value = "/deletar", method = RequestMethod.DELETE)
-	public String delete(@ModelAttribute Long id, Model model) {
+	@RequestMapping(value = "/deletar{id}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable Long id, Model model) {
 		cartorioService.deleteCartorio(id);
+		model.addAttribute("cartorios", cartorioService.getAllCartorios());
 		return "browser";
 	}
 	
