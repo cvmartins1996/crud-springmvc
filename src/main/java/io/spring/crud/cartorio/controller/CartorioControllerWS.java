92d2f8092f92df5f0f2a44e1d88a5a749aef704c
cvmartins1996 <cvmartins1996@gmail.com>
2018-05-20 21:55:27 -0300

    rest

--- src/main/java/io/spring/crud/cartorio/controller/CartorioControllerWS.java
+++ src/main/java/io/spring/crud/cartorio/controller/CartorioControllerWS.java
@@ -3,31 +3,34 @@ package io.spring.crud.cartorio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.spring.crud.cartorio.model.Cartorio;
 import io.spring.crud.cartorio.service.CartorioService;
 
 @RestController
+@CrossOrigin
 public class CartorioControllerWS {
 	
 	private CartorioService cartorioService;
-	private Cartorio cartorio;
-
+	
 	@Autowired
 	public CartorioControllerWS(CartorioService cartorioService) {
 		this.cartorioService = cartorioService;
 	}
 	
 	@RequestMapping(value = "/api-todos")
-	@ResponseBody
 	public List<Cartorio> getTodosCartorios(){
-		cartorio = new Cartorio();
-		cartorio.setId(1l);
-		cartorio.setNome("sao vicente");
-		cartorioService.insertCartorio(cartorio);
 		return cartorioService.getAllCartorios();
 	}
+	
+	@RequestMapping(value = "/inserir")
+	@CrossOrigin
+	public void insertCartorio(@RequestBody Cartorio cartorio) {
+		cartorioService.insertCartorio(cartorio);
+	}
+	
 }
